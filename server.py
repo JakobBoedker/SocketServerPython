eae5b8faec3d57267eaee4187913a391e697eae1
JakobBoedker <bdkerjakob1@gmail.com>
2019-11-27 21:22:47 +0100

    Making some more functions and shit

--- server.py
+++ server.py
@@ -22,9 +22,29 @@ def acceptIncommingConnections():
     while True:
         client, client_address = SERVER.accept()
         print("%s:%s has connected." % client_address)
-        client.send(bytes("Welcome to the server" + "Type your name and press enter!", "utf-8"))
+        client.send(bytes("Welcome to the server" + "Type your name and press enter!", "utf8"))
         addresses[client] = client_address
         Thread(target=handle_client, args=(client, )).start
 
 def handle_client(client): #Tager 'client' som et agument.
-    """Står for den  som et .
+    """Står for den enkelte clients forbindelse"""
+    name = client.recv(BUFSIZ).decode("utf8")
+    welcome = "Velkommen %s! Hvis du vil quite tryk {quit}" % name
+    client.send(bytes(welcome, "utf8"))
+    msg = "%s har tilsluttet til serveren" % name
+    brodcast(bytes(msg, "utf8"))
+    clients[client] = name
+    while True:
+        msg = client.recv(BUFSIZ)
+        if msg != bytes("{quit}", "utf8"):
+            brodcast(msg, name+": ")
+        else:
+            client.send(bytes("{quit}","utf8"))
+            client.close()
+            del clients[client]
+            brodcast(bytes("%s har forladt chatten" % name, "utf8"))
+            break
+def brodcast(msg, prefix=""):
+    """Brodscaster en besked til alle personerne """
+    for sock in clients:
+        sock.send(bytes(prefix, "utf8")+msg)
