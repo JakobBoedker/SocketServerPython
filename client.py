83bd377759ae8fa25216565ce167fce374facbdc
JakobBoedker <bdkerjakob1@gmail.com>
2019-12-06 21:10:29 +0100

    server and client done

--- client.py
+++ client.py
@@ -20,10 +20,54 @@ def send(event=None):
     client_socket.send(bytes(msg, "utf8"))
     if msg == "{quit}":
         client_socket.close()
-        top.close()
+        top.quit()
 
 
 def socket_closing(event=None):
     """Lukker Socket før GUI"""
     my_msg.set("{quit}")
     send()
+
+top = tkinter.Tk()
+top.title("Chatter")
+
+message_frame = tkinter.Frame(top)
+my_msg = tkinter.StringVar() # til at få beskederne sendt
+my_msg.set("Skriv din besked her.")
+scrollbar = tkinter.Scrollbar(message_frame) #Til at se forrige beskeder
+
+msg_list = tkinter.Listbox(message_frame, height=15, width=50, yscrollcommand=scrollbar.set)
+scrollbar.pack(side=tkinter.RIGHT, fill=tkinter.Y)
+msg_list.pack(side=tkinter.LEFT, fill=tkinter.BOTH)
+msg_list.pack()
+
+message_frame.pack()
+
+entry_field = tkinter.Entry(top, textvariable=my_msg)
+entry_field.bind("<Return>", send)
+entry_field.pack()
+send_button = tkinter.Button(top, text="Send", command=send)
+send_button.pack()
+
+top.protocol("VM_DELETE_WINDOW", socket_closing)
+
+
+
+HOST = input("Enter host: ")
+PORT = input("Enter port: ")
+
+if not PORT:
+    PORT = 33000
+else:
+    PORN = int(PORT)
+
+
+BUFSIZ = 1024
+ADDR = (HOST, PORT)
+
+client_socket = socket(AF_INET, SOCK_STREAM)
+client_socket.connect(ADDR)
+
+receive_thread = Thread(target=receive)
+receive_thread.start()
+tkinter.mainloop()
